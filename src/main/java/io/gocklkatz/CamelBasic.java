eb3e5a92c18ca338e96927ccf5543b60cfa5fdc1
Stefan Katzensteiner <stefan.katzensteiner@mailbox.org>
2024-09-24 18:09:59 +0200

    Basic camel route example

---
+++ src/main/java/io/gocklkatz/CamelBasic.java
@@ -0,0 +1,27 @@
+package io.gocklkatz;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.impl.DefaultCamelContext;
+
+public final class CamelBasic {
+
+    public static void main(String[] args) throws Exception {
+        try (CamelContext camel = new DefaultCamelContext()) {
+            camel.addRoutes(createBasicRoute());
+            camel.start();
+
+            Thread.sleep(10_000);
+        }
+    }
+
+    static RouteBuilder createBasicRoute() {
+        return new RouteBuilder() {
+            @Override
+            public void configure() {
+                from("timer:foo")
+                        .log("Hello Camel");
+            }
+        };
+    }
+}
