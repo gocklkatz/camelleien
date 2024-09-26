b3d0d9f275a846ad24fecbd435e0bca4f492e9ca
Stefan Katzensteiner <stefan.katzensteiner@mailbox.org>
2024-09-26 17:42:30 +0200

    Streamlined example

--- src/main/java/io/gocklkatz/camelimp/Camelimp.java
+++ src/main/java/io/gocklkatz/camelimp/Camelimp.java
@@ -6,30 +6,28 @@ import org.apache.camel.impl.DefaultCamelContext;
 
 public class Camelimp {
 
-    public static void main(String[] args) {
-        try (CamelContext camelContext = new DefaultCamelContext()) {
+    public static void main(String[] args) throws Exception {
+        CamelContext camelContext = new DefaultCamelContext();
 
-            camelContext.addRoutes(new RouteBuilder() {
-                @Override
-                public void configure() {
-                    /*
-                    from("timer://foo?fixedRate=true&period=1000")
-                            .log("Loggit!");
-                     */
-                    from("file:///Users/katzi/Downloads/ain")
-                            .process(exchange -> {
-                                Object o = exchange.getIn().getBody();
-                                System.out.println("o = " + o);
-                            })
-                            .to("file:///Users/katzi/Downloads/aout");
-                }
-            });
-            camelContext.start();
+        camelContext.addRoutes(new RouteBuilder() {
+            @Override
+            public void configure () {
+                /*
+                from("timer://foo?fixedRate=true&period=1000")
+                        .log("Loggit!");
+                 */
+                from("file:///Users/katzi/Downloads/ain")
+                        .process(exchange -> {
+                            Object o = exchange.getIn().getBody();
+                            System.out.println("o = " + o);
+                        })
+                        .to("file:///Users/katzi/Downloads/aout");
+            }
+        });
 
-            Thread.sleep(30_000);
+        camelContext.start();
+        Thread.sleep(10_000);
 
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        camelContext.stop();
     }
 }
