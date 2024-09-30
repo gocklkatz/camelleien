e344b3f8d2754df56d56266ef06bf0f830ef26f0
Stefan Katzensteiner <stefan.katzensteiner@mailbox.org>
2024-09-30 19:27:02 +0200

    Clean up

--- src/main/java/io/gocklkatz/camelimp/Camelouting.java
+++ src/main/java/io/gocklkatz/camelimp/Camelouting.java
@@ -1,12 +1,9 @@
 package io.gocklkatz.camelimp;
 
-import jakarta.jms.ConnectionFactory;
-import org.apache.activemq.ActiveMQConnectionFactory;
 import org.apache.camel.CamelContext;
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
-import org.apache.camel.component.jms.JmsComponent;
 import org.apache.camel.impl.DefaultCamelContext;
 
 public class Camelouting {
@@ -14,14 +11,18 @@ public class Camelouting {
     public static void main(String[] args) throws Exception {
         CamelContext context = new DefaultCamelContext();
 
-        ConnectionFactory connectionFactory =
-                new ActiveMQConnectionFactory("vm://localhost");
-        context.addComponent("jms",
-                JmsComponent.jmsComponentAutoAcknowledge(connectionFactory));
-
         context.addRoutes(new RouteBuilder() {
             @Override
             public void configure() {
+
+                from("timer://foo?fixedRate=true&period=1000")
+                        .process(new Processor() {
+                            public void process(Exchange exchange) throws Exception {
+                                System.out.println("Timer triggered.");
+                            }
+                        });
+
+                /*
                 from("ftp://localhost?username=ftpuser&password=sososecret")
                         .process(exchange -> {
                             System.out.println(
@@ -29,11 +30,12 @@ public class Camelouting {
                             );
                         })
                         .to("jms:queue:incomingOrders");
+                 */
             }
         });
 
         context.start();
-        Thread.sleep(10000);
+        Thread.sleep(5_000);
         context.stop();
     }
 }
